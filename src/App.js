05b02f67e057030e55818d34a54275f53b726506
mehddy1 <82449539+mehddy1@users.noreply.github.com>
2022-06-15 01:37:37 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import logo from './LogoFluency2.png';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h2>Une agence de design de marque + expérience pour ceux qui ont de l'ambition.</h2>
+      </header>
+    </div>
+  );
+}
+
+export default App;
